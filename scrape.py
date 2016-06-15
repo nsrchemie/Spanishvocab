a1a75e53d2eca8a67f28548bf4193ba69a8b3fd2
nsrchemie <nsrchemie@gmail.com>
2016-06-15 14:01:11 -0400

    initial scrape set-up

---
+++ scrape.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+r = requests.get('http://lingolex.com/swom/wom-herbs.htm')
+soup = BeautifulSoup(r.content, 'lxml')
+g_data = soup.find_all('td')
+for d in g_data:
+	print(d)
