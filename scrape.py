28b66e05924634685d1c26bade2ddf35e705974b
nsrchemie <nsrchemie@gmail.com>
2016-06-15 14:17:48 -0400

    fixed parser argument

--- scrape.py
+++ scrape.py
@@ -3,7 +3,8 @@ import requests
 
 
 r = requests.get('http://lingolex.com/swom/wom-herbs.htm')
-soup = BeautifulSoup(r.content, 'lxml')
+soup = BeautifulSoup(r.content, 'html.parser')
 g_data = soup.find_all('td')
+
 for d in g_data:
 	print(d)
