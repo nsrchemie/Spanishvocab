f1a4ffd25f97ae29f22951376d3cfca96a64d112
nsrchemie <nsrchemie@gmail.com>
2016-06-15 15:50:40 -0400

    data contains only text

--- scrape.py
+++ scrape.py
@@ -5,5 +5,6 @@ import requests
 r = requests.get('http://lingolex.com/swom/wom-herbs.htm')
 soup = BeautifulSoup(r.content, 'html.parser')
 g_data = soup.find_all('td')
-for a in g_data:
-	print(a.text)
+g_data = [a.text for a in g_data]
+for d in g_data:
+	print(d)
