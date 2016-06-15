0be2dfc9a4d4945e0305e6f35ddf6dc4d8a14996
nsrchemie <nsrchemie@gmail.com>
2016-06-15 16:52:48 -0400

    formatted output for labeling

--- scrape.py
+++ scrape.py
@@ -5,6 +5,6 @@ import requests
 r = requests.get('http://lingolex.com/swom/wom-herbs.htm')
 soup = BeautifulSoup(r.content, 'html.parser')
 g_data = [a.text for a in soup.find_all('td')]
-g_data = iter([d for d in g_data[3:] if d.isupper() == False])
+g_data = iter([d for d in g_data[1:] if d.isupper() == False])
 for d in g_data:
-		print(d, '=>', next(g_data))
+		print(' Spanish: ',d, '\n', 'English: ', next(g_data), '\n')
