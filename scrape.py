4e4cb219212a7820d29d8929c18fe5661036288a
nsrchemie <nsrchemie@gmail.com>
2016-06-15 16:40:28 -0400

    refactored iter conversion

--- scrape.py
+++ scrape.py
@@ -4,9 +4,7 @@ import requests
 
 r = requests.get('http://lingolex.com/swom/wom-herbs.htm')
 soup = BeautifulSoup(r.content, 'html.parser')
-# g_data = soup.find_all('td')
 g_data = [a.text for a in soup.find_all('td')]
-g_data = [d for d in g_data[3:] if d.isupper() == False]
-g_data = iter(g_data)
+g_data = iter([d for d in g_data[3:] if d.isupper() == False])
 for d in g_data:
 		print(d, '=>', next(g_data))
