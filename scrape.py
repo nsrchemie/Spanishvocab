b9659c2f28290837336f5444087cea2721695052
nsrchemie <nsrchemie@gmail.com>
2016-06-15 16:39:42 -0400

    refactored findall

--- scrape.py
+++ scrape.py
@@ -4,10 +4,9 @@ import requests
 
 r = requests.get('http://lingolex.com/swom/wom-herbs.htm')
 soup = BeautifulSoup(r.content, 'html.parser')
-g_data = soup.find_all('td')
-g_data = [a.text for a in g_data]
+# g_data = soup.find_all('td')
+g_data = [a.text for a in soup.find_all('td')]
 g_data = [d for d in g_data[3:] if d.isupper() == False]
-# g_data = g_data[3:]
 g_data = iter(g_data)
 for d in g_data:
 		print(d, '=>', next(g_data))
