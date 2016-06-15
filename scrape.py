07087ab97224b7481ae5616d29a4e550923e2d11
nsrchemie <nsrchemie@gmail.com>
2016-06-15 17:33:08 -0400

    made column names dynamic

--- scrape.py
+++ scrape.py
@@ -7,15 +7,16 @@ import pandas as pd
 r = requests.get('http://lingolex.com/swom/wom-herbs.htm')
 soup = BeautifulSoup(r.content, 'html.parser')
 g_data = [a.text for a in soup.find_all('td')]
-g_data = iter([d for d in g_data[3:] if d.isupper() == False])
+iter_data = iter([d for d in g_data[3:] if d.isupper() == False])
+col1 = g_data[1]
+col2 = g_data[2]
 # for d in g_data:
 # 		print(' Spanish: ',d, '\n', 'English: ', next(g_data), '\n')
-
 data = defaultdict(list)
 
-for d in g_data:
-	data['Spanish'].append(d)
-	data['English'].append(next(g_data))
+for d in iter_data:
+	data[col1].append(d)
+	data[col2].append(next(iter_data))
 
 df = pd.DataFrame(data)
 
