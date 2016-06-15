f18ae1382025c3448da73c5172542c9ceee73252
nsrchemie <nsrchemie@gmail.com>
2016-06-15 15:03:34 -0400

    tags cleaned up, getting relevant text now with html tags

--- scrape.py
+++ scrape.py
@@ -5,6 +5,5 @@ import requests
 r = requests.get('http://lingolex.com/swom/wom-herbs.htm')
 soup = BeautifulSoup(r.content, 'html.parser')
 g_data = soup.find_all('td')
-
-for d in g_data:
-	print(d)
+for a in g_data:
+	print(a.text)
