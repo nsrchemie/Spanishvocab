debd620f4de047930ef971618e3a1943abfa6324
nsrchemie <nsrchemie@gmail.com>
2016-06-15 17:27:13 -0400

    created loop to add scraped info to dataframe with correctly labeled column names

--- scrape.py
+++ scrape.py
@@ -1,10 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
+from collections import defaultdict
+import pandas as pd
 
 
 r = requests.get('http://lingolex.com/swom/wom-herbs.htm')
 soup = BeautifulSoup(r.content, 'html.parser')
 g_data = [a.text for a in soup.find_all('td')]
-g_data = iter([d for d in g_data[1:] if d.isupper() == False])
+g_data = iter([d for d in g_data[3:] if d.isupper() == False])
+# for d in g_data:
+# 		print(' Spanish: ',d, '\n', 'English: ', next(g_data), '\n')
+
+data = defaultdict(list)
+
 for d in g_data:
-		print(' Spanish: ',d, '\n', 'English: ', next(g_data), '\n')
+	data['Spanish'].append(d)
+	data['English'].append(next(g_data))
+
+df = pd.DataFrame(data)
+
+print(df.head())
+
+
+
+
